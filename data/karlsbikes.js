8b2d5841155354e8b747817b17a490e27186ee94
Karl Sigel <heykarl@gmail.com>
2021-10-06 15:35:18 -0700

    changed image for multiple bikes

--- data/karlsbikes.js
+++ data/karlsbikes.js
@@ -23,7 +23,7 @@ module.exports = [
     model: '2500 Grand Tour',
     color: 'Black (now white)',
     type_id: 1,
-    img: 'https://1.bp.blogspot.com/-UQOVL6nUhck/W6pf97lsptI/AAAAAAAArwk/bGDKwHufUV8lBOIGrEFRGxDxcy3rW7FLQCLcBGAs/s1600/Gary%2BFisher%2BReview.jpg',
+    img: 'https://c2.staticflickr.com/4/3876/14897345559_b4059fd696_c.jpg',
     rideable: false
   },
   {
@@ -32,7 +32,7 @@ module.exports = [
     model: 'F800 Furio',
     color: 'Blue w/ flames',
     type_id: 2,
-    img: 'https://figinibike.com/wp-content/uploads/2018/03/37-F800-ALL-MOUNTAIN-FURIO-750.jpg',
+    img: 'https://bikeimages.blob.core.windows.net/bikeimages/cac55e79-2ffb-466c-9609-27373d79ebc3.png',
     rideable: false
   },
   {
@@ -41,7 +41,7 @@ module.exports = [
     model: 'Everest',
     color: 'Yellow (now black)',
     type_id: 2,
-    img: 'https://digitalhippie.net/wp-content/uploads/2011/01/1988-mtb-tour-apollo-everest-550.jpg',
+    img: 'https://farm6.staticflickr.com/5737/22717767397_8dd7011839_c.jpg',
     rideable: false
   },
   {
@@ -56,10 +56,10 @@ module.exports = [
   {
     year: 2000,
     make: 'San Eagle',
-    model: '?',
+    model: 'Crap',
     color: 'Blue',
     type_id: 4,
-    img: 'https://3.bp.blogspot.com/-cgr3v3ZzHQM/UilJMLMYphI/AAAAAAAADOI/zeN2DDCwCu0/s1600/2028-blue+2.JPG',
+    img: 'https://p.globalsources.com/IMAGES/PDT/B1022994886/Folding-Bikes-with-16-x-1-75-Inches-Tire.jpg',
     rideable: true
   }
 ];
